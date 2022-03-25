aed16adb16b9b95efeecbf0009f998a71cca2b41
Eric Damtoft <edamtoft@gmail.com>
2022-03-25 00:38:53 -0400

    Add NPC Label

--- source/DealerOn.Metaverse.Web/ClientApp/src/npc.ts
+++ source/DealerOn.Metaverse.Web/ClientApp/src/npc.ts
@@ -1,9 +1,17 @@
-import { Actor, CollisionType, Engine, Input, PostCollisionEvent, Side } from "excalibur";
+import { CollisionType, Engine, Text } from "excalibur";
 import { PlayerBase } from "./playerBase";
 
 export class Npc extends PlayerBase {
+  private label: Text;
+  
   constructor(name: string, x: number, y: number) {
     super(name, x, y, "Blue", CollisionType.Active);
+    this.label = new Text({ text: name.substring(0, 6) });
+  }
+
+  onInitialize(_engine: Engine): void {
+    super.onInitialize(_engine);
+    this.graphics.onPostDraw = ctx => this.label.draw(ctx, -20, -40);
   }
 
   onPreUpdate(_engine: Engine, _delta: number): void {
