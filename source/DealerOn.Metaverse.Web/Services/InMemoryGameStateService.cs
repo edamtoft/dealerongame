e13e5d2c066ad4bb9135ce3fba8877b29a900b1b
Eric Damtoft <edamtoft@gmail.com>
2022-03-30 23:12:34 -0400

    Improve sharding to be based on actual active users

--- source/DealerOn.Metaverse.Web/Services/InMemoryGameStateService.cs
+++ source/DealerOn.Metaverse.Web/Services/InMemoryGameStateService.cs
@@ -1,23 +1,36 @@
 ﻿using DealerOn.Metaverse.Web.Contracts;
 using DealerOn.Metaverse.Web.Models;
 using System.Collections.Concurrent;
+using System.Collections.Immutable;
 
 namespace DealerOn.Metaverse.Web.Services
 {
   public sealed class InMemoryGameStateService : IGameStateService
   {
-    private const int PlayersPerRoom = 250;
-    private volatile int _id = 0;
+    private const int PlayersPerRoom = 50;
+    private int _id = 0;
+    private int _currentRoom = 0;
+    private int _roomOccupancy = 0;
+    private readonly object _lock = new();
     private readonly ConcurrentDictionary<string, PlayerRegistration> _players = new();
 
     public PlayerRegistration Register(string connection)
     {
       return _players.GetOrAdd(connection, _ =>
       {
-        var id = Interlocked.Increment(ref _id);
-        var room = id / PlayersPerRoom;
-        var playerId = id % PlayersPerRoom;
-        return new PlayerRegistration(room, playerId);
+        lock(_lock)
+        {
+          if (_roomOccupancy >= PlayersPerRoom)
+          {
+            _roomOccupancy = 0;
+            _id = 0;
+            _currentRoom++;
+          }
+
+          _id++;
+          _roomOccupancy++;
+          return new PlayerRegistration(_currentRoom, _id);
+        }
       });
     }
 
@@ -29,7 +42,18 @@ namespace DealerOn.Metaverse.Web.Services
 
     public bool TryRemove(string connection, out PlayerRegistration registration)
     {
-      return _players.TryRemove(connection, out registration);
+      var removed= _players.TryRemove(connection, out registration);
+      if (removed)
+      {
+        lock (_lock)
+        {
+          if (registration.Room == _currentRoom)
+          {
+            _roomOccupancy--;
+          }
+        }
+      }
+      return removed;
     }
   }
 }
